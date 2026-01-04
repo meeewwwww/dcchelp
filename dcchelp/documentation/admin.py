039fa5de1ceeec598e5680928e8237427caf237d
Anna Demchenko <dav_1000@mail.ru>
2026-01-04 14:20:16 +0300

    Added DocumentArticleAdmin, DocumentTypesAdmin, DocumentSubTypeAdmin

--- dcchelp/documentation/admin.py
+++ dcchelp/documentation/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DocumentArticle, DocumentTypes, DocumentSubType
+
+
+@admin.register(DocumentArticle)
+class DocumentArticleAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(DocumentTypes)
+class DocumentTypesAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(DocumentSubType)
+class DocumentSubTypeAdmin(admin.ModelAdmin):
+    ...
